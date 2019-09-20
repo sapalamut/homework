37643ff8cd9b8052ec696be3b768771491945cb0
sapalamut <babayanf@gmail.com>
2019-09-20 10:35:02 +0400

    adding homework 3 to repo

---
+++ HomeWork_3.py
@@ -0,0 +1,14 @@
+# Ex.1
+items_in_fridge = 'fruits'
+no_electricity = 4
+take_all_out = True
+items_in_fridge = int(5)
+your_turn = (items_in_fridge >= no_electricity and take_all_out)
+print(f'if {your_turn} take all fruits out please!')
+
+# Ex.2
+player_condition = True 
+weather = 'good'
+num_of_players = 11
+we_are_playing = (weather == 'good' and player_condition and num_of_players >= 11)
+print(f'Yes, it\'s {we_are_playing} as the weather is {weather}.')
