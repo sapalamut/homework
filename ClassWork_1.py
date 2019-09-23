77e85c0718be48cbb026be0b59ac2b00f7f97613
sapalamut <babayanf@gmail.com>
2019-09-23 20:55:39 +0400

    adding to repo

---
+++ ClassWork_1.py
@@ -0,0 +1,12 @@
+name = input('Please enter your name: ')
+birthdate = int(input('Please enter the year you were born: '))
+age = 2019 - birthdate
+years_100 = 100 - age
+result = print(f'You will become 100 years old in {str(years_100)} years')
+
+# Ex:2
+
+triangle_short_side_1 = input('Enter the first side value: ')
+triangle_short_side_2 = input('Enter the second side value: ')
+triangle_long_side = input('Enter the third side value: ')
+create_triangle = print(f'You can create a triangle {int(triangle_short_side_1) + int(triangle_short_side_2) > int(triangle_long_side) and int(triangle_short_side_1) + int(triangle_long_side) > int(triangle_short_side_2) and int(triangle_long_side) + int(triangle_short_side_2) > int(triangle_short_side_1)}')
