996dbdefc032c18fe1e452e345de461e684bbbc2
sapalamut <babayanf@gmail.com>
2019-09-25 19:40:03 +0400

    adding classwork2 to Repo

---
+++ ClassWork_2.py
@@ -0,0 +1,30 @@
+# Conditionals
+
+a = 3
+b = 4
+if a > b:
+	print(True)
+if a < b:
+	print(False)
+
+c = True
+b = False
+if c == True:
+	print(not False)
+else:
+	print(True)	
+
+
+num_one = (15 + 15)
+num_two = (20 - 10)
+if num_one > num_two:
+	print(True)
+else:
+	print(False)
+
+age = 37
+name = 'Arman'
+if name == 'Arman'and age == 37:
+	print(f'Your name is {name} and your age is {age}')
+else:
+	print('Who are you???')
