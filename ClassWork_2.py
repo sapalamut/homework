bcf472ab68d5c8de60c2426703063389f5b3f7ba
sapalamut <babayanf@gmail.com>
2019-09-25 20:14:45 +0400

    nested if

--- ClassWork_2.py
+++ ClassWork_2.py
@@ -1,5 +1,6 @@
 # Conditionals
 
+# Sample:1
 a = 3
 b = 4
 if a > b:
@@ -7,6 +8,7 @@ if a > b:
 if a < b:
 	print(False)
 
+# Sample:2
 c = True
 b = False
 if c == True:
@@ -14,7 +16,7 @@ if c == True:
 else:
 	print(True)	
 
-
+# Sample:3
 num_one = (15 + 15)
 num_two = (20 - 10)
 if num_one > num_two:
@@ -22,9 +24,23 @@ if num_one > num_two:
 else:
 	print(False)
 
+# Sample:4
 age = 37
 name = 'Arman'
 if name == 'Arman'and age == 37:
 	print(f'Your name is {name} and your age is {age}')
 else:
 	print('Who are you???')
+
+# Sample:5
+weather = 'sunny'
+condition = not False
+random = 'lencheck'
+len_of = len(random)
+print(len_of)
+if weather == 'sunny':
+	print(f'Yeah it\'s{weather} outside!')
+	if condition == True:
+		print(f'So {condition}')
+
+# Sample:6
