bff8b70295c10f90060c14ee2427a9fee21400c6
sapalamut <babayanf@gmail.com>
2019-09-27 19:25:39 +0400

    adding classwork3 to repo

---
+++ ClassWork_3.py
@@ -0,0 +1,24 @@
+# For Loops
+
+# Ex:1
+for i in range(0,10):
+	print(i)
+
+for i in range(2,17):
+	print(i)
+
+for i in range(10,11):
+	print(i)
+
+# Ex:2
+for i in range(10):
+	print(i)
+
+for i in range(0,10):
+	print(i)
+
+for i in range(0,10,2):
+	print(i)
+
+for i in range(10,1,-1):
+	print(i)
