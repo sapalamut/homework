5e25ad42bb66881e157cb4040cdf378880517707
sapalamut <babayanf@gmail.com>
2019-09-27 21:05:58 +0400

    adding another example

--- ClassWork_3.py
+++ ClassWork_3.py
@@ -48,18 +48,68 @@ while i < 10:
 
 # Ex:6
 
-hidden_number = random.randint(1,100)
+#number_of_games = int(input('Number of games'))
+#for i in range(0, number_of_games):
+#	hidden_number = random.randint(1,100)
 
-user_guess	=  0
-while not user_guess == hidden_number:
+#user_guess	=  0
+#while not user_guess == hidden_number:
 
-	user_guess = int(input('Guess the number: '))
+#	user_guess = int(input('Guess the number: '))
 
-	if user_guess > hidden_number:
-		print('Too High!')
+#	if user_guess > hidden_number:
+#		print('Too High!')
 
-	elif user_guess < hidden_number:
-		print('Too Low!')
+#	elif user_guess < hidden_number:
+#		print('Too Low!')
 
-	else:
-		print('That\'s Right!')		
+#	else:
+#		print('That\'s Right!')
+
+# Ex:7
+for i in range(1, 11):
+	for j in range(1, 11):
+		print(i, 'times', j, 'equals', i*j)
+
+# Ex:8
+for i in range(0, 12):
+	if i % 2 == 0:
+		continue
+	print(i)	
+
+# Ex:9
+for i in range(0, 12):
+	if i % 2 == 0:
+		pass
+	else:	
+		print(i)		
+
+# Ex:10
+string = "eqweqeretwrqweqweeqweqweqw"
+
+for i in string:
+	if i == 't':
+	 	break
+	print(i)			
+
+#string = "fdfkjfkdsfnksnkndskandksanda"
+#amount_of_ns = 0
+#	for i in string:
+#		amount_of_ns =+ 1
+#	print(i)
+
+# Ex:11
+
+amount = int(input('Select a number: '))
+
+count = 0
+for i in range(1, amount):
+	
+	for j in range(2, amount):
+		
+		if i % j == 0 and i != j:
+			break
+		if j == amount -1:
+			print(i)
+			count += 1
+print(count)				
