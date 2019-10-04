107dfaed6dce6e14a0257b49a90a974f0572237a
sapalamut <babayanf@gmail.com>
2019-10-04 20:55:49 +0400

    changes added

--- ClassWork_6.py
+++ ClassWork_6.py
@@ -52,3 +52,40 @@ try:
 		input_file.close()
 except IOError:
 	print("An error occured reading the file!")		
+
+# Ex4.
+def zero_division():
+	print(1 / 0)
+
+try:	
+	zero_division()
+except Exception as error:
+	print(error)
+
+# Ex5.
+while True:
+	try:
+		x = int(input('Enter a number: '))
+		y = int(input('Enter another number: '))
+		print(x, '/',y, '=', x/y)
+		break 
+	except ZeroDivisionError:
+		print("Can't devide by zero!")
+	except ValueError:
+		print("That doesn't look like a number")
+	except:
+		print("Something unexpected happened!")			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
