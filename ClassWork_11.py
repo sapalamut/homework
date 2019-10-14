677417126ddb04081a3d4455d33f8ea44661552a
sapalamut <babayanf@gmail.com>
2019-10-14 20:50:04 +0400

    adding changes

--- ClassWork_11.py
+++ ClassWork_11.py
@@ -57,5 +57,45 @@ classes['Math'].append('Jirayr')
 print('Students in math class', classes['Math'])
 
 # Ex:4
+info_data = {
+	"worker": ['David', 'Mcloud'],
+	"age": [21,]
+}
+
+print('Workers info is', info_data['worker'])
+info_data['age'].append(50)
+
+print('Workers age is', info_data['age'])
+
+# Ex:5
+sample_text = 'Far far away behind the word mountains far from the countries Vokalia and Consonantia there live the blind texts Separated they live in Bookmarksgrove right at the coast of the Semantics a large language ocean A small river named Duden flows by their place and supplies it with the necessary regelialia It is a paradisematic country in which roasted parts of sentences fly into your mouth Even the all powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar The Big Oxmox advised her not to do so because there were thousands of bad Commas'
+
+word_dict = {}
+
+sample_text = sample_text.lower()
+sample_text = sample_text.split(' ')
+
+for word in sample_text:
+	if word in word_dict.keys():
+		word_dict[word] += 1
+	else:
+		word_dict[word] = 1
+
+
+for (word, amount) in word_dict.items():
+	if amount > 2:
+		print(word, ":", amount)			
+
+
+
+
+
+
+
+
+
+
+
+
 
 
