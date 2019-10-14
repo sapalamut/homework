83c9326da4eda3c823ed87308fd7ce3a06a62c0e
sapalamut <babayanf@gmail.com>
2019-10-14 19:48:17 +0400

    adding changes

--- ClassWork_11.py
+++ ClassWork_11.py
@@ -1,11 +1,11 @@
 # Dictionaries
-human = {'name': 'Jirayr', 'surname': 'Mekilyan', 'age': 19, 'is_married': False}
+human = {'name': 'Jirayr', 'surname': 'Melikyan', 'age': 19, 'is_married': False}
 print(human['name'], human['surname'], human['is_married'])
 
 #adding
 human['age'] = 30
 
-#changingS
+#changing
 human['have_pet'] = False
 
 #deleting
@@ -13,4 +13,16 @@ del human['age']
 
 print(human)
 
+for value in human.values():
+	print(value)
+
+print(human.keys())
+print(human.values())
+
+# Ex:1
+fruits = {'apple': 5, 'orange': 14, 'pinapple': 1, 'banannas': 4, 'pomegranade': 16}
+
+for key in fruits.keys():
+	if fruits[key] > 5:
+		print(key)
 
