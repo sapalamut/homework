2f8b02ed61e86efaf58c8be2d038b81d18268540
sapalamut <babayanf@gmail.com>
2019-10-14 19:30:49 +0400

    adding changes

--- ClassWork_11.py
+++ ClassWork_11.py
@@ -1,3 +1,16 @@
 # Dictionaries
 human = {'name': 'Jirayr', 'surname': 'Mekilyan', 'age': 19, 'is_married': False}
 print(human['name'], human['surname'], human['is_married'])
+
+#adding
+human['age'] = 30
+
+#changingS
+human['have_pet'] = False
+
+#deleting
+del human['age']
+
+print(human)
+
+
